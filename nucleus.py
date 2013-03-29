344fbc4f57395c080b18ba9b44d2fffb053046bd
defm03 <yutsuro@gmail.com>
2013-03-29 16:17:52 +0100

    Proton, neutron and electron var update - creating charge type and num by new class

--- nucleus.py
+++ nucleus.py
@@ -21,33 +21,36 @@ electron_symb = ["e-","beta-"]  # putchar method in python lib -sys
 proton_mass_kg = 1.672621 * pow(10,-27) #kg
 proton_mass_u = 1.007276 #atomic mass unit
 proton_e_charge = "+1" # e = elementary charge
+proton_charge = charge("+", 1)
 proton_eC_charge = 1.602176 * pow(10,-19) #C
 #const values for neutron
 neutron_mass_kg = 1.674927 * pow(10,-27) #kg
 neutron_mass_u = 1.008664 #atomic mass unit
 neutron_e_charge = "0e" # e = elementary charge
+neutron_charge = charge("0", 0)
 neutron_eC_charge = 0 #C
 #const values for electron
 electron_mass_kg = 9.109382 * pow(10,-31) #kg
 electron_mass_u1 = 5.485799 * pow(10,-4) #u
 electron_e_charge = "-1" # e = elementary charge
+electron_charge = charge("-", 1)
 electron_eC_charge = -1.602176 * pow(10,-19)
 
 class charge:
   def __init__(self, chrg_type="", chrg_number=0):
     self.chrg_type = chrg_type
     self.chrg_number = chrg_number
-    
+
     if chrg_type == "+":
       chrg_type = positive_charge
     if chrg_type == "-":
       chrg_type = negative_charge
-    
-    if chrg_num <= 1:
-      chrg_type = charged
 
+    if chrg_num >= 1:
+      chrg_type = charged
     if chrg_number == 0:
       chrg_type = non_charge
+    
     if chrg_type == "0":
       chrg_type = non_charge
 
