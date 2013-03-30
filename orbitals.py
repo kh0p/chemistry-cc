e207b450c6201d59c8cb7cb0e0aa4119aebab6ca
defm03 <yutsuro@gmail.com>
2013-03-30 17:20:43 +0100

    added: start up var

--- orbitals.py
+++ orbitals.py
@@ -8,8 +8,10 @@
 from random import *
 x = 0.0
 y = 0.0
-x_s = 1.0,-1.0
-y_s = 1.0,-1.0
+z = 0.0
+x_s = 1.0,1.0,1.0
+y_s = 1.0,1.0,1.0
+z_s = 1.0,1.0,1.0
 plane = [[x][y]]
 def rand_pos_s(x_s,y_s):
   random.randrange(x_s,y_s)
