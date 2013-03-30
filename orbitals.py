a585d2c6297e3632215669c3996f03baaa75f95c
defm03 <yutsuro@gmail.com>
2013-03-30 15:22:46 +0100

    setting up orbitals

---
+++ orbitals.py
@@ -0,0 +1,19 @@
+# s orbitals
+# p orbitals
+# d,f orbitals
+# on plane - 2D model
+from random import *
+x = 0.0
+y = 0.0
+x_s = 1.0,-1.0
+y_s = 1.0,-1.0
+plane = [[x][y]]
+def rand_pos_s(x_s,y_s):
+  random.randrange(x_s,y_s)
+def rand_pos_p():
+  #shi-- that's gonna be hard
+  pass
+
+class Orbital:
+  def __init__(self):
+    pass
