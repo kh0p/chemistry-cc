cb0053f9a0c321f2b15b40bbedc11cf4ad8688f6
defm03 <yutsuro@gmail.com>
2013-03-31 20:07:05 +0200

    cleaning up -added u def in units

--- units.py
+++ units.py
@@ -18,7 +18,14 @@ class unit_type:
     else:
       pass
 km = unit_type('Kilometre', 'km', 1.0)
-m = unit_type('metre', 'm', 1.0)
+m = unit_type('Metre', 'm', 1.0)
 km_to_m = m.val * pow(10,3)
 #km.val = m.val * pow(10,3)
+g = unit_type('Gram', 'g', 1.0)
+#to count >> atoms in 12grams of C-12
+#1 mole
+mole_C_12 = 6.022 * pow(10,23) #12grams
+mole_O    = 6.022 * pow(10,23) #16grams // O amu=16
+#  MOLAR MASS (M) in gram = RELATIVE ATOMIC MASS (RAM) in amu
+u = unit_type('units', 'u', 1.66*pow(10,-27)) # val in kg
 
