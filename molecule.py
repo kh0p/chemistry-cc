8f2a961cc36d6e06b2040134968efbd75c209e21
defm03 <yutsuro@gmail.com>
2013-04-01 09:54:00 +0200

    setting up chem_el_xx fror local var in Molecule

--- molecule.py
+++ molecule.py
@@ -1,12 +1,16 @@
 from atom import *
 atom_list = {}
 class Molecule:
-  def __init__(self, chem_el_x, full_molecule = ""):
-    self.atom_list = atom_list
+  def __init__(self, chem_el_xx, full_molecule = ""):
+    self.chem_el_xx = chem_el_xx
+    #chem_el_xx = local;
+    #damn; kinda wierd way to do it
+    chem_el_xx == chem_el_x
     self.full_molecule = full_molecule
-    atom_list[chem_el_x.name] = chem_el_x.num_of_atoms
+    atom_list[chem_el_xx.name] = chem_el_xx.num_of_atoms
+    full_molecule == str(chem_el_xx.name) + str(chem_el_xx.num_of_atoms)
 
 H_molecule = atom([1,0],1)
 H_molecule.chem_el_x(H,2)
-H_molecule_out = Molecule()
+H_molecule_out = Molecule(H,"")
 
