24997e120c89e47530e570827b33c05f5637d7bb
defm03 <yutsuro@gmail.com>
2013-04-01 09:41:25 +0200

    atoms > chem_el_x; molecule __init__

--- atom.py
+++ atom.py
@@ -1,5 +1,7 @@
 from nucleus import *
 from orbitals import *
+from periodic_table import *
+down_indx = 0
 class atom:
   def __init__(self, nucleus=[0,0],electrons=0):
     #nucleus[0] == protons
@@ -8,4 +10,8 @@ class atom:
     self.electrons = electrons
     
     self.electrons == self.nucleus[0]
-
+  def chem_el_x(self, name="",num_of_atoms=0):
+    self.num_of_atoms = num_of_atoms
+    self.name = name
+    name == chemical_element.name
+    num_of_atoms == down_indx
--- molecule.py
+++ molecule.py
@@ -1,6 +1,12 @@
 from atom import *
-
+atom_list = {}
 class Molecule:
-  def __init__(self, atoms=[]):
-    pass
+  def __init__(self, chem_el_x, full_molecule = ""):
+    self.atom_list = atom_list
+    self.full_molecule = full_molecule
+    atom_list[chem_el_x.name] = chem_el_x.num_of_atoms
+
+H_molecule = atom([1,0],1)
+H_molecule.chem_el_x(H,2)
+H_molecule_out = Molecule()
 
