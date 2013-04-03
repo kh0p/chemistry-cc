c343d439574802fb7e808579faa1c35a6504f59d
defm03 <yutsuro@gmail.com>
2013-04-03 17:43:33 +0200

    Charge -checking for neg; -new file + init class

---
+++ charge.py
@@ -0,0 +1,16 @@
+from nucleus import *
+neg_list = []
+class Charge_mig:
+  def __init__(self, to_neg, negative_chrg_number):
+  #to_neg = there should be x protons
+  #and x electrons; y of neutrons
+    self.negative_chrg_number = negative_chrg_number
+    self.to_neg = to_neg
+    to_neg == proton.charge
+    if to_neg == negative_chrg_number:
+      neg_q = True
+      neg_list.append(neg_q)
+    if to_neg != negative_chrg_number:
+      neg_q = False
+      neg_list.append(neg_q)
+
